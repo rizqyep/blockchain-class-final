166240346c6804587372c3b39b9b99683c86d0d3
Rizqy EP <rizqyepr@gmail.com>
2022-07-08 16:59:57 +0700

    feat: manually added constructor to insert todo

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) tasks;
+
+    constructor() public {
+        createTask("Init new task added");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
