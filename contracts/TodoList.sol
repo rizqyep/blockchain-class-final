bd6c7ae518d9c4f981fc39fbeaa46bd01df42796
Rizqy EP <rizqyepr@gmail.com>
2022-07-08 18:19:46 +0700

    feat: added completion feature for todo list

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -22,4 +22,10 @@ contract TodoList {
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
     }
+
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+    }
 }
