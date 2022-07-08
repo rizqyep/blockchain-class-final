c236ee6a2e74fd11889e090ff53ead258088f7d7
Rizqy EP <rizqyepr@gmail.com>
2022-07-08 17:49:00 +0700

    feat: added event emitter on contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,8 @@ contract TodoList {
 
     mapping(uint256 => Task) public tasks;
 
+    event TaskCreated(uint256 id, string content, bool completed);
+
     constructor() public {
         createTask("Init new task added");
     }
@@ -18,5 +20,6 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
